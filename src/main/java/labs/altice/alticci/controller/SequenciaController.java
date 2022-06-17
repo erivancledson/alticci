af4f60ba51f1baf2a7ca136358d9529df9897cf7
Erivan Cledson <erivancled@hotmail.com>
2022-06-16 21:31:50 -0300

    swagger

--- src/main/java/labs/altice/alticci/controller/SequenciaController.java
+++ src/main/java/labs/altice/alticci/controller/SequenciaController.java
@@ -3,6 +3,7 @@ package labs.altice.alticci.controller;
 import labs.altice.alticci.dto.SequenciaDTO;
 import labs.altice.alticci.service.SequenciaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/alticci")
+@CrossOrigin(origins = "*")
 public class SequenciaController {
 
     @Autowired
